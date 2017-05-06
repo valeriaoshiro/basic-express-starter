fb9138a96ea603b8797e6cfc58373d237c3fa353
Ben Junya <bjunya@gmail.com>
2017-05-06 15:21:35 -0700

    Detail view

---
+++ __tests__/applicantspage-tests.js
@@ -0,0 +1,1 @@
+applicantspage-tests.js
---
+++ ui/js/components/applicantsPage.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class ApplicantsPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.displayName = 'ApplicantsPage';
+    }
+
+    render() {
+        let userData = this.props.userData;
+        return (
+            <div>
+                <div>
+                    <h2>{userData.fullname || this.props.username}</h2>
+                    <a className="" href={userData.html_url}>{`@${userData.login}`}</a>
+                    <p>Followers: <span>{userData.followers}</span></p>
+                    <p>Repositories: <span>{userData.public_repos}</span></p>
+                    <img src={userData.avatar_url} />
+                    <br />
+                    {this.props.repos.map( (item, idx) => {
+                        return (
+                            <p key={idx}>
+                                <a href={item.html_url}>{item.name}</a>
+                            </p>
+                        );
+                    })}
+                </div>
+            </div>
+        );
+    }
+
+
+}
+
+ApplicantsPage.propTypes = {
+    username: PropTypes.string.isRequired,
+    userData: PropTypes.object.isRequired,
+    repos: PropTypes.array.isRequired
+};
+
+export default ApplicantsPage;
