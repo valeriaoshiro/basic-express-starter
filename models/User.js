bc8c900762826e4f54e858075061e5b3d8e41064
Mohammad Mohammad <mohammad@elephantdrive.com>
2017-05-06 13:16:33 -0700

    fixed bcrypt library

--- models/User.js
+++ models/User.js
@@ -33,12 +33,10 @@ userSchema.pre('save', function(next) {
   // only hash the password if it has been modified (or is new)
   if (!user.isModified('password')) return next();
   // generate a salt
-  bcrypt.genSalt(10, function(err, salt) {
-    if (err) return next(err);
-    // hash the password using our new salt
-    bcrypt.hash(user.password, salt, function(err, hash) {
-      if (err) return next(err);
-      // override the cleartext password with the hashed one
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) { return next(err); }
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      if (err) { return next(err); }
       user.password = hash;
       next();
     });
